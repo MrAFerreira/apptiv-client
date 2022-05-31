a5466ed90a600c56b8a2ef0e0c084321b9d79f41
Pipokinha <pipokinha@superpro.email>
2022-05-31 11:41:16 +0100

    created avatar component

---
+++ src/components/Avatar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const AvatarTag = styled.img`
+border-radius: 50%;
+display: inline-block;
+border-style: none;
+height: ${(props) => props.width || '56px'};
+width: ${(props) => props.width || '56px'};
+`;
+
+function Avatar({image, width}) {
+  return (
+    
+    <AvatarTag src={image} width={width} alt='avatar'/>
+  )
+}
+
+export default Avatar
