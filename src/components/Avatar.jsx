676e243066d782620da2ffc228331f0965a22ee5
joaoGon <jpgon@live.com.pt>
2022-05-31 11:52:37 +0100

    checked

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,19 +1,16 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
 const AvatarTag = styled.img`
-border-radius: 50%;
-display: inline-block;
-border-style: none;
-height: ${(props) => props.width || '56px'};
-width: ${(props) => props.width || '56px'};
+  border-radius: 50%;
+  display: inline-block;
+  border-style: none;
+  height: ${(props) => props.width || "56px"};
+  width: ${(props) => props.width || "56px"};
 `;
 
-function Avatar({image, width}) {
-  return (
-    
-    <AvatarTag src={image} width={width} alt='avatar'/>
-  )
+function Avatar({ image, width }) {
+  return <AvatarTag src={image} width={width} alt="avatar" />;
 }
 
-export default Avatar
+export default Avatar;
