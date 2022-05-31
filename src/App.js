9613f15575f4688458aca77b3cf3317a9558c6ed
Anna Egger <anna.3gg3r@gmail.comgit usernamegit config --global user.email anna.3gg3r@gmail.com>
2022-05-31 17:42:07 +0100

    styles login page

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
-import './App.css';
-import { Routes, Route } from 'react-router-dom';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
 //Package imports
-import EventsCard from './components/EventsCard';
+import EventsCard from "./components/EventsCard";
 
 //Components and Pages imports
-import Appbar from './components/Appbar';
-import Header from './components/Header';
-import MutlipageForm from './components/MutlipageForm';
+import Appbar from "./components/Appbar";
+import Header from "./components/Header";
+import MutlipageForm from "./components/MutlipageForm";
 /* Welcome/initialize pages */
-import WelcomePage from './pages/WelcomePage';
-import SplashScreenPage from './pages/SplashScreenPage';
-import AboutPage from './pages/AboutPage';
+import WelcomePage from "./pages/WelcomePage";
+import SplashScreenPage from "./pages/SplashScreenPage";
+import AboutPage from "./pages/AboutPage";
 /* Auth pages */
-import SignUpFormPage from './pages/SignUpFormPage';
-import SignUpOnBoardingPage from './pages/SignUpOnBoardingPage';
-import LoginPage from './pages/LoginPage';
+import SignUpFormPage from "./pages/SignUpFormPage";
+import SignUpOnBoardingPage from "./pages/SignUpOnBoardingPage";
+import LoginPage from "./pages/LoginPage";
 /* Events pages */
-import EventListPage from './pages/EventListPage';
-import EventDetailsPage from './pages/EventDetailsPage';
-import EventCreationPage from './pages/EventCreationPage';
-import BookingsPage from './pages/BookingsPage';
+import EventListPage from "./pages/EventListPage";
+import EventDetailsPage from "./pages/EventDetailsPage";
+import EventCreationPage from "./pages/EventCreationPage";
+import BookingsPage from "./pages/BookingsPage";
 /* Statistic pages */
-import ProgressPage from './pages/ProgressPage';
+import ProgressPage from "./pages/ProgressPage";
 /* Error page */
-import ErrorPage from './pages/ErrorPage';
+import ErrorPage from "./pages/ErrorPage";
 
 function App() {
   return (
@@ -48,12 +48,6 @@ function App() {
         <Route path="*" element={<ErrorPage />} />
       </Routes>
       <Appbar />
-      <EventsCard
-        title="yoga"
-        startDate="24. May 2022"
-        price="3"
-        image="https://images.unsplash.com/photo-1639333586638-fb4e185084b8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8"
-      />
     </div>
   );
 }
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -2,6 +2,12 @@ import { useState, useContext } from "react";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../context/auth.context";
+import styled from "styled-components";
+import ButtonBottom from "../components/ButtonBottom";
+
+import appleLogo from "../assets/images/social-media-signup.svg";
+import gitHubLogo from "../assets/images/icon-park_github-one.svg";
+import googleLogo from "../assets/images/icon-park_google.svg";
 
 function LoginPage(props) {
   const [email, setEmail] = useState("");
@@ -32,29 +38,96 @@ function LoginPage(props) {
       });
   };
 
+  const H1 = styled.h1`
+    font-style: normal;
+    font-weight: 1000;
+    font-size: 36px;
+    line-height: 50px;
+    text-align: center;
+  `;
+
+  const Input = styled.input`
+    background: #ffffff;
+    border: 1px solid #c2c2c2;
+    border-radius: 10px;
+    width: 342px;
+    height: 50px;
+    margin-bottom: 0.5rem;
+  `;
+  const Label = styled.label`
+    font-style: normal;
+    font-weight: 1000;
+    font-size: 15px;
+    line-height: 21px;
+    letter-spacing: -0.24px;
+    color: #ebb220;
+    text-align: left;
+  `;
+
+  const FormContainer = styled.div`
+    text-align: left;
+    padding: 0 1rem;
+
+    .sign-up-container {
+      text-align: center;
+    }
+    button {
+      margin: 1rem 0;
+    }
+  `;
+
+  const Button = styled(ButtonBottom)`
+    margin: 1rem 0;
+  `;
+
+  const LogoContainer = styled.div`
+    display: flex;
+    justify-content: space-around;
+
+    .logo {
+      width: 64px;
+      height: 64px;
+      color: white;
+      background: #ebb220;
+      border-radius: 54px;
+    }
+    .signup-p {
+      text-align: center !important;
+    }
+  `;
+
   return (
-    <div>
-      <h1>Login</h1>
+    <FormContainer>
+      <H1>Login</H1>
 
       <form onSubmit={handleLoginSubmit}>
-        <label>Email address:</label>
-        <input type="email" name="email" value={email} onChange={handleEmail} />
+        <Label>Email address:</Label>
+        <Input type="email" name="email" value={email} onChange={handleEmail} />
 
-        <label>Password:</label>
-        <input
+        <Label>Password:</Label>
+        <Input
           type="password"
           name="password"
           value={password}
           onChange={handlePassword}
         />
 
-        <button type="submit">Continue</button>
+        <Button type="submit">Continue</Button>
       </form>
+      <p>or log in with</p>
+      <LogoContainer>
+        <img className="logo" src={appleLogo} />
+        <img className="logo" src={gitHubLogo} />
+        <img className="logo" src={googleLogo} />
+      </LogoContainer>
       {errorMessage && <p className="error-message">{errorMessage}</p>}
-
-      <p>Don't have an account yet?</p>
-      <Link to={"/signup"}> Sign Up</Link>
-    </div>
+      <div className="sign-up-container">
+        <p className="signup-p">Don't have an account yet?</p>
+        <Link to={"/signup"} style={{ color: "black" }}>
+          Sign Up
+        </Link>
+      </div>
+    </FormContainer>
   );
 }
 
