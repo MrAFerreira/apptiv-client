67bde665818cb25b0efcb763a557737413d6f140
Anna Egger <anna.3gg3r@gmail.comgit usernamegit config --global user.email anna.3gg3r@gmail.com>
2022-05-31 09:52:16 +0100

    starts EventCard

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import './App.css';
+import "./App.css";
 //Package imports
+import EventsCard from "./components/EventsCard";
 
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 
 //Components and Pages imports
-import Appbar from './components/Appbar';
-import Homepage from './pages/Homepage';
+import Appbar from "./components/Appbar";
+import Homepage from "./pages/Homepage";
 
 function App() {
   return (
@@ -14,6 +15,12 @@ function App() {
         <Route path="/" element={<Homepage />} />
       </Routes>
       <Appbar />
+      <EventsCard
+        title="yoga"
+        startDate="24. May 2022"
+        price="3"
+        image="https://images.unsplash.com/photo-1639333586638-fb4e185084b8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8"
+      />
     </div>
   );
 }
---
+++ src/components/EventsCard.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import styledComponents from 'styled-components'
+import styled from styledComponents;
+
+function EventsCard(props) {
+    const { image, title, startDate, price } = props;
+
+
+const Cardtag = styled.div`
+width: 300px;
+display: flex;
+flex-direction: column;
+gap: 15px;
+align-items: center;
+border-radius: 15px;
+
+.title {
+  text-align: center;
+}
+
+.cardImage {
+    object-fit: cover;
+}
+`;
+
+
+  return (
+      <Cardtag>
+          <div>
+          <img className='cardImage' src={image} />
+          </div>
+          <div className='date-price-container'>
+              <span>{startDate}</span> <span>{price} €</span>
+              <h3>{ title}</h3>
+          </div>
+          
+          
+  </Cardtag>
+  )
+}
+
+export default EventsCard
