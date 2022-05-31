ee620977933680c0aa19650e9be575431766618a
MrAFerreira <andre.f.v.ferreira@gmail.com>
2022-05-31 18:04:29 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
-import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
 //Package imports
-import EventsCard from "./components/EventsCard";
+import EventsCard from './components/EventsCard';
 
 //Components and Pages imports
-import Appbar from "./components/Appbar";
-import Header from "./components/Header";
-import MutlipageForm from "./components/MutlipageForm";
+import Appbar from './components/Appbar';
+import Header from './components/Header';
+import MutlipageForm from './components/MutlipageForm';
 /* Welcome/initialize pages */
-import WelcomePage from "./pages/WelcomePage";
-import SplashScreenPage from "./pages/SplashScreenPage";
-import AboutPage from "./pages/AboutPage";
+import WelcomePage from './pages/WelcomePage';
+import SplashScreenPage from './pages/SplashScreenPage';
+import AboutPage from './pages/AboutPage';
 /* Auth pages */
-import SignUpFormPage from "./pages/SignUpFormPage";
-import SignUpOnBoardingPage from "./pages/SignUpOnBoardingPage";
-import LoginPage from "./pages/LoginPage";
+import SignUpFormPage from './pages/SignUpFormPage';
+import SignUpOnBoardingPage from './pages/SignUpOnBoardingPage';
+import LoginPage from './pages/LoginPage';
 /* Events pages */
-import EventListPage from "./pages/EventListPage";
-import EventDetailsPage from "./pages/EventDetailsPage";
-import EventCreationPage from "./pages/EventCreationPage";
-import BookingsPage from "./pages/BookingsPage";
+import EventListPage from './pages/EventListPage';
+import EventDetailsPage from './pages/EventDetailsPage';
+import EventCreationPage from './pages/EventCreationPage';
+import BookingsPage from './pages/BookingsPage';
 /* Statistic pages */
-import ProgressPage from "./pages/ProgressPage";
+import ProgressPage from './pages/ProgressPage';
 /* Error page */
-import ErrorPage from "./pages/ErrorPage";
+import ErrorPage from './pages/ErrorPage';
 
 function App() {
   return (
@@ -34,7 +34,7 @@ function App() {
           <Route path="/welcome" element={<WelcomePage />} />
           <Route path="/about" element={<AboutPage />} />
           <Route path="/signup" element={<SignUpFormPage />} />
-          <Route path="/onboarding-signup" element={<SignUpOnBoardingPage />} />
+          <Route path="/onboarding-signup" element={<MutlipageForm />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/event-list" element={<EventListPage />} />
           <Route path="/event-details/:id" element={<EventDetailsPage />} />
