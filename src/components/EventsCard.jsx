efaf7dca52ce086201187a6fb4024474dfa127dc
Anna Egger <anna.3gg3r@gmail.comgit usernamegit config --global user.email anna.3gg3r@gmail.com>
2022-05-31 14:02:45 +0100

    adds interest-card and interest-container components

--- src/components/EventsCard.jsx
+++ src/components/EventsCard.jsx
@@ -1,42 +1,37 @@
-import React from 'react'
-import styledComponents from 'styled-components'
-import styled from styledComponents;
+import React from "react";
+import styled from "styled-components";
 
 function EventsCard(props) {
-    const { image, title, startDate, price } = props;
+  const { image, title, startDate, price } = props;
 
+  const Cardtag = styled.div`
+    width: 300px;
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
+    align-items: center;
+    border-radius: 15px;
 
-const Cardtag = styled.div`
-width: 300px;
-display: flex;
-flex-direction: column;
-gap: 15px;
-align-items: center;
-border-radius: 15px;
+    .title {
+      text-align: center;
+    }
 
-.title {
-  text-align: center;
-}
-
-.cardImage {
-    object-fit: cover;
-}
-`;
-
+    .cardImage {
+      object-fit: cover;
+    }
+  `;
 
   return (
-      <Cardtag>
-          <div>
-          <img className='cardImage' src={image} />
-          </div>
-          <div className='date-price-container'>
-              <span>{startDate}</span> <span>{price} €</span>
-              <h3>{ title}</h3>
-          </div>
-          
-          
-  </Cardtag>
-  )
+    <Cardtag>
+      <div>
+        <img className="cardImage" src={image} />
+      </div>
+      <div className="date-price-container">
+        <span>{startDate}</span> <span>{price} €</span>
+        <h3>{title}</h3>
+      </div>
+    </Cardtag>
+  );
 }
 
-export default EventsCard
+export default EventsCard;
--- src/pages/WelcomePage.jsx
+++ src/pages/WelcomePage.jsx
@@ -1,10 +1,12 @@
 //This is a dummy component. There will not be a Homepage (the main page is the events page)
 import apptivLogo from "../assets/images/apptiv-logo.svg";
+import InterestComponent from "../components/InterestComponent";
 
 function WelcomePage() {
   return (
     <div>
       <img src={apptivLogo} alt="apptiv logo" />
+      <InterestComponent />
     </div>
   );
 }
