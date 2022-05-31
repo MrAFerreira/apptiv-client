08b9200488caad844f8c6b3073fbe68f0cb1f3db
MrAFerreira <andre.f.v.ferreira@gmail.com>
2022-05-31 10:29:30 +0100

    formatting changes

--- src/index.js
+++ src/index.js
@@ -1,39 +1,39 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter as Router } from "react-router-dom";
-import { AuthProviderWrapper } from "./context/auth.context";
-import { ThemeProvider } from "styled-components";
-import GlobalStyle from "./styles/GlobalStyles";
-import FontStyles from "./styles/fontStyles";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { AuthProviderWrapper } from './context/auth.context';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from './styles/GlobalStyles';
+import FontStyles from './styles/fontStyles';
 
 const theme = {
   colors: {
-    mainOrange: "#EBB220",
-    black: "#000000",
-    white: "#FFFFFF",
-    textBlack: "#282828",
-    darkGrey: "#48494B",
-    lightGrey: "#C8C8C9",
-    readMoreGrey: "#949494",
-    jan: "#017BBB",
-    feb: "#0199D3",
-    mar: "#009E4F",
-    apr: "#74AE36",
-    may: "#ACC91B",
-    jun: "#EBAA12",
-    jul: "#E7880B",
-    aug: "#E93510",
-    sep: "#E02B51",
-    oct: "#B6347A",
-    nov: "#6D4097",
-    dec: "#45409A",
+    mainOrange: '#EBB220',
+    black: '#000000',
+    white: '#FFFFFF',
+    textBlack: '#282828',
+    darkGrey: '#48494B',
+    lightGrey: '#C8C8C9',
+    readMoreGrey: '#949494',
+    jan: '#017BBB',
+    feb: '#0199D3',
+    mar: '#009E4F',
+    apr: '#74AE36',
+    may: '#ACC91B',
+    jun: '#EBAA12',
+    jul: '#E7880B',
+    aug: '#E93510',
+    sep: '#E02B51',
+    oct: '#B6347A',
+    nov: '#6D4097',
+    dec: '#45409A',
   },
 };
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
