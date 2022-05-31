cc6d56c4e7453d3f7508dbebac70260b76dd1cee
Dino Marchiori <dinaco1@gmail.com>
2022-05-31 17:03:25 +0100

    fixed service api calls

--- src/service/user.services.js
+++ src/service/user.services.js
@@ -1,40 +1,38 @@
 import axios from "axios";
 
 class UserService {
-    constructor () {
-        this.api = axios.create({
-            baseUrl: process.env.REACT_APP_BASE_URL || "http://localhost:5005", 
-        });
+  constructor() {
+    this.api = axios.create({
+      baseUrl: process.env.REACT_APP_BASE_URL || "http://localhost:5005",
+    });
     // Automatically set JWT token in the headers for every request
     this.api.interceptors.request.use((config) => {
-    // Retrieve the JWT token from the local storage
-    const storedToken = localStorage.getItem("authToken");
+      // Retrieve the JWT token from the local storage
+      const storedToken = localStorage.getItem("authToken");
 
-    if (storedToken) {
-      config.headers = { Authorization: `Bearer ${storedToken}` };
-    }
+      if (storedToken) {
+        config.headers = { Authorization: `Bearer ${storedToken}` };
+      }
 
-    return config;
-  });
+      return config;
+    });
+  }
 
-    }
+  currentUser = async (userId) => {
+    return this.api.get(`/api/user/${userId}`);
+  };
 
-    currentUser = async () => {
-        return this.api.get("/api/user/{username}");
-      };
-    
-      updateCurrentUser = async (requestBody) => {
-        return this.api.put("/api/user/{username}", requestBody);
-      };
+  updateCurrentUser = async (userId, requestBody) => {
+    return this.api.put(`/api/user/${userId}`, requestBody);
+  };
 
-      deleteCurrentUser = async (requestBody) => {
-        return this.api.delete("/api/user/{username}", requestBody);
-      };
+  deleteCurrentUser = async (userId, requestBody) => {
+    return this.api.delete(`/api/user/${userId}`, requestBody);
+  };
 
-      getCurrentUserFriends = async () => {
-        return this.api.get("/api/user/{username}/friends");
-      };
-
+  getCurrentUserFriends = async (userId) => {
+    return this.api.get(`/api/user/${userId}/friends`);
+  };
 }
 
 // Create one instance of the service
