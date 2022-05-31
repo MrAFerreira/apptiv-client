7eb64b4089d8dcb55e85554d462e156ae554a011
Matilde Costa <matildecosta@MacBook-Pro-de-Matilde.local>
2022-05-31 14:12:04 +0100

    create event ongoing

--- src/pages/EventCreationPage.jsx
+++ src/pages/EventCreationPage.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
 
 function EventCreation() {
   return (
-    <div>EventCreation</div>
-  )
+    <section className="create-event-main">
+      <h3>Create an event</h3>
+      <form>
+        <label htmlFor="title">Title*</label>
+        <input type="text" name="title" id="title" />
+
+        <label htmlFor="description">Description*</label>
+        <input type="text" name="description" id="description" />
+
+        <label htmlFor="location">Location*</label>
+        <input type="" name="location" id="location" />
+
+        <label htmlFor="start-date">Start date and time*</label>
+        <input type="datetime-local" name="start-date" id="start-date" />
+      </form>
+    </section>
+  );
 }
 
-export default EventCreation
+export default EventCreation;
