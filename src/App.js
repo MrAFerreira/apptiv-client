62781e4b81b64259658859348e99f36a79c0d9b1
Matilde Costa <matildecosta@MacBook-Pro-de-Matilde.local>
2022-05-31 10:15:56 +0100

    routes created

--- src/App.js
+++ src/App.js
@@ -5,13 +5,40 @@ import { Routes, Route } from "react-router-dom";
 
 //Components and Pages imports
 import Appbar from "./components/Appbar";
+/* Welcome/initialize pages */
 import WelcomePage from "./pages/WelcomePage";
+import SplashScreenPage from "./pages/SplashScreenPage";
+import AboutPage from "./pages/AboutPage";
+/* Auth pages */
+import SignUpFormPage from "./pages/SignUpFormPage";
+import SignUpOnBoardingPage from "./pages/SignUpOnBoardingPage";
+import LoginPage from "./pages/LoginPage";
+/* Events pages */
+import EventListPage from "./pages/EventListPage";
+import EventDetailsPage from "./pages/EventDetailsPage";
+import EventCreationPage from "./pages/EventCreationPage";
+import BookingsPage from "./pages/BookingsPage";
+/* Statistic pages */
+import ProgressPage from "./pages/ProgressPage";
+/* Error page */
+import ErrorPage from "./pages/ErrorPage";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<WelcomePage />} />
+        <Route path="/" element={<SplashScreenPage />} />
+        <Route path="/welcome" element={<WelcomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/signup" element={<SignUpFormPage />} />
+        <Route path="/onboarding-signup" element={<SignUpOnBoardingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/event-list" element={<EventListPage />} />
+        <Route path="/event-details/:id" element={<EventDetailsPage />} />
+        <Route path="/create-event" element={<EventCreationPage />} />
+        <Route path="/my-bookings" element={<BookingsPage />} />
+        <Route path="/your-progress" element={<ProgressPage />} />
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
       <Appbar />
     </div>
